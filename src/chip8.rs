1080a0a77f542da1ce5788307c67f7ac7eec8907
poci <poci@kali>
2024-08-25 03:09:30 +0200

    Started implemented the chip8 architecture

---
+++ src/chip8.rs
@@ -0,0 +1,55 @@
+pub struct Chip8 {
+    memory: [u8; 4096],
+    pc: u16,
+    index_register: u16,
+    stack: Vec<u16>,
+    delay_timer: u8,
+    sound_timer: u8,
+    registers: [u8; 16],
+}
+
+impl Chip8 {
+    pub fn new() -> Self {
+        Self {
+            memory: [0; 4096],
+            pc: 0,
+            index_register: 0,
+
+            //Font
+            stack: vec![
+                0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+                0x20, 0x60, 0x20, 0x20, 0x70, // 1
+                0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+                0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+                0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+                0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+                0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+                0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+                0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+                0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+                0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+                0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+                0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+                0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+                0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+                0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+            ],
+            delay_timer: 255,
+            sound_timer: 255,
+            registers: [0; 16],
+        }
+    }
+
+    pub fn load_rom(program: Vec<u8>) {}
+
+    pub fn update_timers(&mut self) {
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+        if self.sound_timer > 0 {
+            self.sound_timer -= 1;
+        }
+    }
+
+    pub fn tick(&mut self) {}
+}
