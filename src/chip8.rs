b363b92f501a508dee1e0c3ed30340f36dbfbebe
Matyas Zsombor <mzsombi@icloud.com>
2024-08-27 23:36:13 +0200

    Implemented the screen using sdl2

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,4 +1,7 @@
+use std::fs;
+
 pub struct Chip8 {
+    screen: [bool; 64 * 32],
     memory: [u8; 4096],
     pc: u16,
     index_register: u16,
@@ -8,15 +11,30 @@ pub struct Chip8 {
     registers: [u8; 16],
 }
 
+impl Default for Chip8 {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Chip8 {
     pub fn new() -> Self {
-        Self {
+        let mut chip8 = Self {
             memory: [0; 4096],
+            screen: [true; 64 * 32],
             pc: 0,
             index_register: 0,
 
-            //Font
-            stack: vec![
+            stack: vec![],
+
+            delay_timer: 255,
+            sound_timer: 255,
+            registers: [0; 16],
+        };
+
+        //LOADS FONT STARTING AT 0x50
+        chip8.load_into_memory(
+            vec![
                 0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
                 0x20, 0x60, 0x20, 0x20, 0x70, // 1
                 0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -34,13 +52,22 @@ impl Chip8 {
                 0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
                 0xF0, 0x80, 0xF0, 0x80, 0x80, // F
             ],
-            delay_timer: 255,
-            sound_timer: 255,
-            registers: [0; 16],
-        }
+            0x50,
+        );
+        chip8
     }
 
-    pub fn load_rom(program: Vec<u8>) {}
+    pub fn load_rom(&mut self, file_name: &String) {
+        self.load_into_memory(
+            fs::read(file_name).unwrap_or_else(|_| panic!("Couldn't find file: {}", file_name)),
+            0x200,
+        );
+        println!("{:#02x}", self.memory[0x201])
+    }
+
+    pub fn get_screen(&self) -> &[bool; 64 * 32] {
+        &self.screen
+    }
 
     pub fn update_timers(&mut self) {
         if self.delay_timer > 0 {
@@ -49,6 +76,10 @@ impl Chip8 {
         if self.sound_timer > 0 {
             self.sound_timer -= 1;
         }
+    }
+
+    fn load_into_memory(&mut self, data: Vec<u8>, start_address: usize) {
+        self.memory[start_address..(data.len() + start_address)].copy_from_slice(&data[..]);
     }
 
     pub fn tick(&mut self) {}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,50 @@
+use std::env;
+
+use chip8::Chip8;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use std::time::Duration;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 
 mod chip8;
 
+const SCALE: u32 = 15;
+const WIDTH: u32 = 64;
+const HEIGHT: u32 = 32;
+
 fn main() -> Result<(), String> {
-    println!("Hello World!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        panic!("Example usage: chip8 file.ch8")
+    }
+
+    let mut chip8 = Chip8::new();
+    println!("{}", &args[1]);
+    chip8.load_rom(&args[1]);
 
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("rust-sdl2 demo", 800, 600)
+        .window("chip8", WIDTH * SCALE, HEIGHT * SCALE)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
 
     let mut canvas = window
         .into_canvas()
+        .present_vsync()
         .build()
         .expect("could not make a canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
     canvas.present();
+
     let mut event_pump = sdl_context.event_pump()?;
-    let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -43,10 +57,28 @@ fn main() -> Result<(), String> {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
-
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        draw_screen(&chip8, &mut canvas)
     }
     Ok(())
+}
+
+pub fn draw_screen(chip8: &Chip8, canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+
+    canvas.clear();
+
+    let buffer = chip8.get_screen();
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+
+    for (i, pixel) in buffer.iter().enumerate() {
+        if *pixel {
+            let x = i as u32 % WIDTH;
+            let y = i as u32 / WIDTH;
+
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
+    canvas.present();
 }
